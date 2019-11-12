388503a63da766b8d7a4a49ca8f9f5de37b3505b
chasingboom <chasingboom@users.noreply.github.com>
2019-11-12 02:13:37 -0500

    .edit nav bar

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ app.use(express.json({limit: '5mb'}));
 
 app.get('/api', (request,response)=> {
     console.log('data is here');
-    fs.readFile("public/data/article_nyt.json", (err, data)=>{
+    fs.readFile("public/data/key.json", (err, data)=>{
         if (err) throw err;
         let day = JSON.parse(data);
-        response.json(day)
+        // console.log(day)
+        const keys = Object.keys(day)
+        response.json(keys)
     });
 })
 
@@ -23,11 +25,11 @@ app.post('/req',(request,response)=>{
     console.log(request.body);
     const tryout = request.body;
     // request.body to create link data
-    fs.readFile("public/data/article_nyt.json", (err, data)=>{
+    fs.readFile("public/data/article_nyt.json", (err, info)=>{
         const selected = []
 
         if (err) throw err;
-        let day = JSON.parse(data);
+        let day = JSON.parse(info);
         for(let i = 0; i < day.length; i++){
             for(let j = 0; j< day[i]['keywords'].length; j++){
                 if(day[i]['keywords'][j]['value'] == tryout[0]){
@@ -35,79 +37,137 @@ app.post('/req',(request,response)=>{
                 }
             }
         }
-        console.log(selected)
+        console.log(selected) // all the article that has that keyword
+    
+    const data = {"name":tryout[0],"children":[]}
 
-        //create nodes 
-        const node = []
+    //re draw the data
+    const date = []
+    for(let i = 0; i < selected.length; i ++){
+        date.push(selected[i].date.slice(0,4))
+    }
+    var unique = date.filter((v, i, a) => a.indexOf(v) === i); 
+    // console.log(unique)
+    const final = {"name":tryout[0],"children":[]}
 
-        for(let i = 0; i < selected.length; i++){
-            //year node
-            console.log(selected[i].date.slice(0,4))
-            node.push({
-                "id":selected[i].date.slice(0,4),
-                "group":"year"
-            })
-            //*** make sure the id is not repetitive *****//
-            //https://stackoverflow.com/questions/39885893/how-to-find-duplicate-values-in-a-javascript-array-of-objects-and-output-only-u
-
-            //month node
-            console.log(selected[i].date.slice(5,7))
-            node.push({
-                "id":selected[i].date.slice(5,7),
-                "group":"month"
-            })
-            //headline node
-            console.log(selected[i].headline)
-            node.push({
-                "id":selected[i].headline,
-                "group":"headline"
-            })
-            //keyword node
-            for(let j = 0; j < selected[i].keywords.length; j ++){
-                console.log(selected[i].keywords[j].value)
-                node.push({
-                    "id":selected[i].keywords[j].value,
-                    "group":"keyword"
-                })
+    // const dateData = []
+    for(let i = 0; i < unique.length; i ++){
+        final.children.push({"name":unique[i],"children":[]})
+    }
+    // console.log(final)
+    console.log(final.children[0].name)
+    for(let i = 0; i < final.children.length; i ++){
+       for(let j = 0; j < selected.length; j ++){
+            if(selected[j].date.slice(0,4) == final.children[i].name){
+                final.children[i].children.push({"name":selected[j].headline})
             }
         }
-        // console.log(nodes)
+    }
+   
+    console.log(final)
+
+// ### script for d3 force graph
+        // //create nodes 
+        // const node = []
+
+        // for(let i = 0; i < selected.length; i++){
+        //     //year node
+        //     console.log(selected[i].date.slice(0,4))
+        //     node.push({
+        //         "id":selected[i].date.slice(0,4),
+        //         "group":"year"
+        //     })
+        //     //month node
+        //     console.log(selected[i].date.slice(5,7))
+        //     node.push({
+        //         "id":selected[i].date.slice(5,7),
+        //         "group":"month"
+        //     })
+        //     //headline node
+        //     console.log(selected[i].headline)
+        //     node.push({
+        //         "id":selected[i].headline,
+        //         "group":"headline"
+        //     })
+        //     //keyword node
+        //     for(let j = 0; j < selected[i].keywords.length; j ++){
+        //         // if(selected[i].keywords[j].name == "persons"){
+        //         //     // check if the striing has (
+        //         //         if(selected[i].keywords[j].value.includes("(") == true){
+        //         //             console.log(selected[i].keywords[j].value)
+        //         //             var name = selected[i].keywords[j].value.substring( 0, selected[i].keywords[j].value.indexOf("("));
+        //         //             console.log(name);
+        //         //             var lastName = name.substring( 0, name.indexOf(","));
+        //         //             console.log(lastName);
+        //         //             var firstName = name.substring(name.indexOf(",")+1, name.length());
+        //         //             console.log(firstName)
+        //         //             node.push({
+        //         //                 "id":firstName + " " + lastName,
+        //         //                 "group":"keyword"
+        //         //             })
+        //         //         }else{
+        //         //             var lastName = name.substring( 0, name.indexOf(","));
+        //         //             console.log(lastName);
+        //         //             var firstName = name.substring(name.indexOf(",")+1, name.length());
+        //         //             console.log(firstName)
+        //         //             node.push({
+        //         //                 "id":firstName + " " + lastName,
+        //         //                 "group":"keyword"
+        //         //             })
+        //         //         }
+        //         // }else{
+        //             node.push({
+        //                 "id":selected[i].keywords[j].value,
+        //                 "group":"keyword"
+        //             })
+        //         // }
+        //     }
+        // }
+        // // console.log(nodes)
         
-        nodes = node.filter((set => f => !set.has(f.id) && set.add(f.id))(new Set));
-        console.log(nodes)
+        // nodes = node.filter((set => f => !set.has(f.id) && set.add(f.id))(new Set));
+        // console.log(nodes)
 
-        const links = []
-        //create link
-        for(let i = 0; i < selected.length; i++){
-            //// make sure there is no repeat link
-            //// make sure there is no repeat link
-            links.push({
-                "source": selected[i].date.slice(0,4),
-                "target": selected[i].date.slice(5,7),
-                "id": selected[i].date.slice(0,7),
-                "value": 2
-            })
-            for(let j = 0; j < selected[i].keywords.length; j ++){
-                links.push({
-                    "source": selected[i].date.slice(5,7),
-                    "target": selected[i].keywords[j].value,
-                    "id": selected[i].date,
-                    "value": 2
-                })
-                links.push({
-                    "source": selected[i].keywords[j].value,
-                    "target": selected[i].headline,
-                    "id": selected[i].date,
-                    "value": 2
-                })
-            }
-        }
+        // const links = []
+        // //create link
+        // for(let i = 0; i < selected.length; i++){
+        //     //// make sure there is no repeat link
+        //     //// make sure there is no repeat link
+        //     links.push({
+        //         "source": selected[i].date.slice(0,4),
+        //         "target": selected[i].date.slice(5,7),
+        //         "id": selected[i].date.slice(0,7),
+        //         "value": 2
+        //     })
+        //     for(let j = 0; j < selected[i].keywords.length; j ++){
+        //         links.push({
+        //             "source": selected[i].date.slice(5,7),
+        //             "target": selected[i].keywords[j].value,
+        //             "id": selected[i].date,
+        //             "value": 2
+        //         })
+        //         links.push({
+        //             "source": selected[i].keywords[j].value,
+        //             "target": selected[i].headline,
+        //             "id": selected[i].date,
+        //             "value": 2
+        //         })
+        //     }
+        // }
+// ### end of the script for d3 force graph 
 
-        console.log(links)
-        // change unique to nodes 
-        //send the nodes and link back to response.json
+
+        // console.log(links)
+        // // change unique to nodes 
+        // //send the nodes and link back to response.json
+        // response.json({
+        //     nodes,links
+        // })
+
         response.json({
-            nodes,links
+            // data
+            // strat
+            final
         })
 
     });
--- public/src/main.js
+++ public/src/main.js
@@ -1,139 +1,264 @@
 "use strict";
+
 async function data(){
       
   const response = await fetch('/api'); 
   const data = await response.json();
-  console.log(data)
-  console.log('d3 chart')
+  console.log(data) // list of keyword
 }
+
 data(); 
+  let b = ["Education (K-12)","#MeToo Movement","New York City",
+  "Women and Girls","Blacks","Computers and the Internet","Ethics (Personal)"
+  ,"Television","Labor and Jobs","Democratic Party"]
 
-const d = document.getElementById('Success').innerHTML;
-console.log(d);
+  // create list of button with class btn default 
+  for(let i=0; i < b.length; i ++){
+    var bu = document.createElement("button");
+    bu.id = b[i];
+    bu.innerHTML = b[i];
 
-let key = "Democratic Party"
+    var body = document.getElementById("keyword");
+    body.appendChild(bu);
 
-const button = document.getElementById(`${key}`);
-    button.addEventListener('click', async event => {
-      const data = [document.getElementById(`${key}`).innerHTML];
+    bu.addEventListener ("click", function() {
+      // alert("did something");
+      console.log(b[i]) 
+      show(b[i])
+    });
+  }
+
+// fix this  //
+function searchFunction(){
+  var input, filter, ul, li, a , i; 
+  input = document.getElementById("searchTextbox")
+  filter = input.value
+  ul = document.getElementById('find')
+  li = document.getElementsByTagName('li')
+  for(i = 0; i < li.length; i++){
+    a = li[i].getElementsByTagName('a')[0]
+    if(a.innerHTML.indexOf(filter) > -1){
+      li[i].style.display = '';
+    }else{
+      li[i].style.display = 'none';
+    }
+  }
+}
+
+async function show(data){
+      const key = [document.getElementById(data).innerHTML];
       const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(key)
       };
       const response = await fetch('/req', options);
       const json = await response.json();
       console.log(json);
+      hierarchy(json.final);
+}
 
-      chart(json); 
-    });
-
-
-const chart = (data) => {
+// const chart = (data) => {
     
-    // d3.json("../data/links.json").then(function(data) {
-        const links = data.links
-        const nodes = data.nodes
-        const color =  d3.scaleOrdinal(d3.schemeCategory10);
-        console.log(data);
-        console.log('nodes', nodes)
-        console.log('links', links)
+//     // d3.json("../data/links.json").then(function(data) {
+//         const links = data.links
+//         const nodes = data.nodes
+//         const color =  d3.scaleOrdinal(d3.schemeCategory10);
+//         console.log(data);
+//         console.log('nodes', nodes)
+//         console.log('links', links)
       
       
-        const simulation = d3.forceSimulation(data.nodes)
-            .force("link",d3.forceLink(links).id((d,i) => d.id).distance(200))
-            .force("charge", d3.forceManyBody())
-            .force("x", d3.forceX())
-            .force("y", d3.forceY());
+//         const simulation = d3.forceSimulation(data.nodes)
+//             .force("link",d3.forceLink(links).id((d,i) => d.id).distance(200))
+//             .force("charge", d3.forceManyBody())
+//             .force("x", d3.forceX())
+//             .force("y", d3.forceY());
         
-        const height = window.innerWidth
-        const width = window.innerWidth
-        const svg = d3.select("#forceChart")
-            .append("svg")
-            .attr("viewBox", [-width/2,-height/2, width, height]);
+//         const height = window.innerWidth
+//         const width = window.innerWidth
+//         const svg = d3.select("#forceChart")
+//             .append("svg")
+//             .attr("viewBox", [-width/2,-height/2, width, height]);
       
-        const link = svg.append("g")
-            .attr("stroke", "#999")
-            .attr("stroke-opacity", 0.6)
-          .selectAll("line")
-          .data(links)
-          .join("line")
-          .attr("stroke-width", (d,i)=> (d.value));
-            // .attr("stroke-width", (d,i)=> console.log(Math.sqrt(d[i].value)));
+//         const link = svg.append("g")
+//             .attr("stroke", "#FFF")
+//             .attr("stroke-opacity", 0.6)
+//           .selectAll("line")
+//           .data(links)
+//           .join("line")
+//           .attr("stroke-width", (d,i)=> (d.value));
+//             // .attr("stroke-width", (d,i)=> console.log(Math.sqrt(d[i].value)));
       
-        const drag = simulation => {
+//         const drag = simulation => {
         
-              function dragstarted(d) {
-                if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-                d.fx = d.x;
-                d.fy = d.y;
-              }
+//               function dragstarted(d) {
+//                 if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+//                 d.fx = d.x;
+//                 d.fy = d.y;
+//               }
               
-              function dragged(d) {
-                d.fx = d3.event.x;
-                d.fy = d3.event.y;
-              }
+//               function dragged(d) {
+//                 d.fx = d3.event.x;
+//                 d.fy = d3.event.y;
+//               }
               
-              function dragended(d) {
-                if (!d3.event.active) simulation.alphaTarget(0);
-                d.fx = null;
-                d.fy = null;
-              }
+//               function dragended(d) {
+//                 if (!d3.event.active) simulation.alphaTarget(0);
+//                 d.fx = null;
+//                 d.fy = null;
+//               }
               
-              return d3.drag()
-                  .on("start", dragstarted)
-                  .on("drag", dragged)
-                  .on("end", dragended);
-            }
+//               return d3.drag()
+//                   .on("start", dragstarted)
+//                   .on("drag", dragged)
+//                   .on("end", dragended);
+//             }
         
-        const node = svg.append("g")
-            .attr("stroke", "#fff")
-            // .attr("stroke-width", 1.5)
-          .selectAll("circle")
-          .data(data.nodes)
-          .join("circle")
-            .attr("r", 5)
-            .style("fill", d => color(d.group))
-            .call(drag(simulation));
+//         const node = svg.append("g")
+//             .attr("stroke", "#fff")
+//             // .attr("stroke-width", 1.5)
+//           .selectAll("circle")
+//           .data(data.nodes)
+//           .join("circle")
+//             .attr("r", 5)
+//             .style("fill", d => color(d.group))
+//             .call(drag(simulation));
         
-          node.append("svg:title")
-            .attr("dx", 12)
-            .attr("dy", ".35em")
-            .text(function(d) { return d.id });
+//           node.append("svg:title")
+//             .attr("dx", 12)
+//             .attr("dy", ".35em")
+//             .text(function(d) { return d.id });
       
-          var labels = svg.append("g")
-            .attr("class", "label")
-          .selectAll("text")
-          .data(data.nodes)
-          .enter().append("text")
-            .attr("dx", 6)
-            .attr("dy", ".35em")
-             .style("font-size",10)
-            .text(function(d) { return d.id });
+//           var labels = svg.append("g")
+//             .attr("class", "label")
+//           .selectAll("text")
+//           .data(data.nodes)
+//           .enter().append("text")
+//             .attr("dx", 6)
+//             .attr("dy", ".35em")
+//              .style("font-size",10)
+//             // .text(function(d) { return d.id });
       
-        simulation.on("tick", () => {
-          link
-              .attr("x1", d => d.source.x)
-              .attr("y1", d => d.source.y)
-              .attr("x2", d => d.target.x)
-              .attr("y2", d => d.target.y);
+//         simulation.on("tick", () => {
+//           link
+//               .attr("x1", d => d.source.x)
+//               .attr("y1", d => d.source.y)
+//               .attr("x2", d => d.target.x)
+//               .attr("y2", d => d.target.y);
       
-          node
-              .attr("cx", d => d.x)
-              .attr("cy", d => d.y);
-              // .attr("transform", function(d) {
-              //   return "translate(" + d.x + "," + d.y + ")";
-              // })
-          labels
-              .attr("x", function(d) { return d.x; })
-              .attr("y", function(d) { return d.y; });  
+//           node
+//               .attr("cx", d => d.x)
+//               .attr("cy", d => d.y);
+//               // .attr("transform", function(d) {
+//               //   return "translate(" + d.x + "," + d.y + ")";
+//               // })
+//           labels
+//               .attr("x", function(d) { return d.x; })
+//               .attr("y", function(d) { return d.y; });  
+//         });
+//     // })
+// };
+
+
+
+// D3 hierarchy chart
+
+const hierarchy = (data) =>{
+  d3.select("svg").remove();
+
+  const radius = 400
+  const tree = d3.tree()
+    .size([2 * Math.PI, radius])
+    .separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth)
+
+  console.log("lets get it")
+  console.log(data)
+  // const stratify = d3.stratify()
+  //               .id((d)=> d.name)
+  //               .parentId((d)=> d.parent);
+
+  // const root = stratify(data)
+  const root = tree(d3.hierarchy(data)
+      .sort((a, b) => d3.ascending(a.data.name, b.data.name)));
+  
+  console.log(root)
+  const height = window.innerWidth
+  const width = window.innerWidth
+
+  const svg = d3.select("#forceChart")
+      .append("svg")
+      .attr("viewBox", [-width/2,-height/3, width, height])
+      .style("font", "10px sans-serif")
+      .style("margin", "5px");
+
+  const link = svg.append("g")
+    .attr("fill", "none")
+    .attr("stroke", "#555")
+    .attr("stroke-opacity", 0.4)
+    .attr("stroke-width", 1.5)
+  .selectAll("path")
+  .data(root.links())
+  // .data(console.log())
+  .join("path")
+    .attr("d", d3.linkRadial()
+        .angle(d => d.x)
+        .radius(d => d.y));
+
+  const node = svg.append("g")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-width", 3)
+  .selectAll("g")
+  .data(root.descendants().reverse())
+  .join("g")
+    .attr("transform", d => `
+      rotate(${d.x * 180 / Math.PI - 90})
+      translate(${d.y},0)
+    `);
+
+  const div = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0)
+
+  node.append("circle")
+    .attr("fill", d => d.children ? "#555" : "#999")
+    .attr("r", 2.5)
+    .on("mouseover", function(d){
+      div.transition()
+        .duration(100)
+        .style("opacity",1)
+        var element = d3.select(this)
+          element.style("fill", "Black")
+          div.html("<span style = 'font-weight: bold'>" 
+          + d.data.name + "<br>")
+          div.style("visibility", "visible")
+          .style("left", (d3.event.pageX - 20) + "px")    
+          .style("top", (d3.event.pageY - 35) + "px")
+        })
+        .on("mousemove", function(d){
+          div.style("left", (d3.event.pageX) + "px")    
+          .style("top", (d3.event.pageY - 35) + "px")
+        })
+        .on("mouseout", function(d){
+          div.transition()
+          .duration(500)
+          div.style("visibility", "hidden")
+          var element = d3.select(this)
+          element.style("fill", d.pixel)
         });
-    // })
-};
 
-// chart();
+  node.filter(function(d){return d.data.parent == null})
+    .append("text")
+    .text((d)=>console.log(d.data.name))
+    .attr("dy", "0.31em")
+    .attr("x", d => d.x < Math.PI === !d.children ? 6 : -6)
+    .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
+    .attr("transform", d => d.x >= Math.PI ? "rotate(180)" : null)
+  .clone(true).lower()
+    .attr("stroke", "white");
+  }
 
-
-
+// hierarchy(h);
